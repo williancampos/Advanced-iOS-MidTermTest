3d236f1cbcdead29d1091b5cdbdcb840421653b4
Willian Campos <willian.campos@gmail.com>
2017-03-07 15:24:24 -0500

    Add initial version of Shopping List

---
+++ MidTermTest/TitleCell.swift
@@ -0,0 +1,15 @@
+//
+//  TitleCell.swift
+//  MidTermTest
+//
+//  Created by Willian Campos on 2017-03-07.
+//  Copyright © 2017 Willian Campos. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UITableViewCell {
+    
+    @IBOutlet weak var listNameTextField: UITextField!
+    
+}
--- MidTermTest/ViewController.swift
+++ MidTermTest/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2;
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch (section) {
+        case 0: return 1;
+        case 1: return 5;
+        default: return 0;
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCell", for: indexPath) as! TitleCell
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+            
+            return cell
+        }
+        
+    }
+    
+    
 }
 
